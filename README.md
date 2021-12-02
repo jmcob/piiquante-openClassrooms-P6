236db594822fab821356471f8cd074108351e19f
jmcob <jmcob@pm.me>
2021-12-02 09:29:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# piiquante - openClassrooms web developer P6 2021
+
+## Introduction
+
+Après un cours openClassrooms de Will Alexander, nous sommes propulsés dans le backend JS avec node.js;
+Nous installons tous les modules nécéssaires au backend dans ce repo :
+
+express, mongoDB, node.js;
+
+helmet, dotenv, multer;
+
+## piiquante est un site d'avis gastronomiques
+
+Nous pouvons ici poster des sauces : une route sauce est ajoutée avec un controller (un middleware);
+
+Nous pouvons aussi y mettre des likes ou des dislikes;
+
+Le site web est authentifiée par utilisateur et mot de passe (route auth);
---
+++ routes/sauces.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+// multer pour enregistrer les images sur le serveur
+const multer = require("../middleware/multer-config");
+
+const saucesCtrl = require("../controllers/sauces");
+// routes d'ajout, consultation, suppression et modification des sauces
+router.get("/", auth, saucesCtrl.getAllSauces);
+router.post("/", auth, multer, saucesCtrl.createSauce);
+router.get("/:id", auth, saucesCtrl.getOneSauce);
+router.put("/:id", auth, multer, saucesCtrl.modifySauce);
+router.delete("/:id", auth, saucesCtrl.deleteSauce);
+// route des likes
+router.post("/:id/like", auth, saucesCtrl.likeSauce);
+
+module.exports = router;
